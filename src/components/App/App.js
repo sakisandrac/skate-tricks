887c3821f0cbda9953049899383bead43147f51e
“Saki <sakisandra.c@gmail.com>
2023-07-24 09:55:44 -0600

    Update trick state through form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import { getTricks } from '../../apiCalls';
 import Card from '../Card/Card';
+import Form from '../Form/Form';
 
 
 function App() {
@@ -19,10 +20,20 @@ function App() {
     return tricks.map(trick => <Card key={trick.id} trick={trick}/>)
   }
 
+  const addTrick = (newTrick) => {
+    setTricks(prev=> [newTrick, ...prev])
+  }
+
+  useEffect(() => {
+    console.log(tricks)
+  }, [tricks])
 
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
+      <div className='form-container'>
+        <Form addTrick={addTrick}/>
+      </div>
       <div className='cards-container'>
         {renderTrickCards()}
       </div>
---
+++ src/components/Form/Form.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react'
+
+const Form = ({ addTrick }) => {
+
+  const [stance, setStance] = useState("");
+  const [name, setName] = useState("");
+  const [obstacle, setObstacle] = useState("");
+  const [link, setLink] = useState("");
+  
+  const handleChange = (e) => {
+    const {value, name} = e.target
+    if (name === 'stance') {
+      setStance(value)
+    }
+
+    if (name === 'name') {
+      setName(value)
+    }
+
+    if (name === 'obstacle') {
+      setObstacle(value)
+    }
+
+    if (name === 'link') {
+      setLink(value)
+    }
+  }
+
+  // useEffect(() => {
+  //   console.log(stance)
+  //   console.log(name)
+  //   console.log(link)
+  //   console.log(obstacle)
+  // },[name, stance, obstacle, link])
+  
+  const submitTrick =(e) => {
+    e.preventDefault();
+    const newTrick = {
+      id: Date.now(),
+      name: name,
+      obstacle: obstacle,
+      stance: stance,
+      tutorial: link
+    }
+
+    addTrick(newTrick)
+  
+  }
+
+  return (
+    <div className='form'>
+      <form>
+          <select name="stance" onChange={(e) => {handleChange(e)}}>
+            <option value="">Choose Your Stance</option>
+            <option value="regular">Regular</option>
+            <option value="switch">Switch</option>
+          </select>
+          <input type="text" name='name' placeholder='Name of Trick' value={name} onChange={(e) => {handleChange(e)}}/>
+          <select name="obstacle" onChange={(e) => {handleChange(e)}}>
+            <option value="">Choose Your Obstacle</option>
+            <option value="flatground">Flatground</option>
+            <option value="ledge">Ledge</option>
+            <option value="rail">Rail</option>
+            <option value="stairs">Stairs</option>
+            <option value="pool">Pool</option>
+          </select>
+          <input type='text' name='link' placeholder='Link to Tutorial' value={link} onChange={(e) => {handleChange(e)}}/>
+          <button onClick={(e) => {submitTrick(e)}}>Send it!</button>
+        </form>
+    </div>
+  )
+}
+
+export default Form
